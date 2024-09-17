d43ad41c0caa3fd0a09fcf865e518f8d2b062fc2
Alessio Barletta <barle.ale@gmail.com>
2024-09-17 18:50:58 +0200

    fixExercise1

--- exercise1.cpp
+++ exercise1.cpp
@@ -7,16 +7,19 @@ int main(int argc, char * argv[]) {
     double a = 0, b, c;
     double r1, r2;
 
-    for(int i = 0 ; i < 7 ; i++)
-        a += (d0 + 1) * pow(10, i);
+    for(int i = 0 ; i < 7 ; i++){
+        a = (d0 + 1) * pow(10, i);
 
-    b = (d1 + 1) * pow(10, 20);
-    c = -b;
+        b = (d1 + 1) * pow(10, 20);
+        c = -b;
 
-    r1 = (a + b) + c;
-    r2 = a + (b + c);
+        r1 = (a + b) + c;
+        r2 = a + (b + c);
 
-    std::cout << "r1 = " << r1 << std::endl << "r2 = " << r2 << std::endl;
+        std::cout << "r1 = " << r1 << std::endl << "r2 = " << r2 << std::endl;
+        std::cout << "--------------" << std::endl;
+    }
+
 
     return 0;
 }
